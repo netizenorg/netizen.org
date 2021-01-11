d371ff334b0c73af3ad38c8834f6f99fd96fa7fd
nbriz <nickbriz@gmail.com>
2021-01-11 14:54:05 -0600

    updated slides

---
+++ www/slides/slides.js
@@ -0,0 +1,44 @@
+const slides = [
+  {
+    video: 'whitney.mp4'
+  },
+  {
+    html: `
+      <img src="images/tate-exhibition.jpeg">
+      <br>
+      <p><a href="https://amaliaulman.eu/" target="_blank">Amalia Ulman</a>'s Excellences & Perfections (2014) at the TATE</p>
+    `
+  },
+  {
+    html: `
+      <img src="images/lonely-girl.jpeg">
+      <br>
+      <p>lonelygirl15 (2006) <a href="https://www.theguardian.com/technology/2016/jun/16/lonelygirl15-bree-video-blog-youtube" target="_blank">article</a></p>
+    `
+  },
+  {
+    html: `
+      <img src="images/keith-obadike.jpeg">
+      <br>
+      <p><a href="https://amaliaulman.eu/" target="_blank">Keith Obadike</a>'s' <a href="https://obadike.tripod.com/ebay.html" target="_blank">Blackness for Sale</a> (2001) <i>archived version</i></p>
+    `
+  },
+  {
+    video: 'rozendaal.mp4'
+  },
+  {
+    video: 'diana-smith-css-art.mp4'
+  },
+  {
+    html: `
+      <h1>video links</h1>
+      <ul>
+        <li><a href="https://en.wikipedia.org/wiki/Christiane_Paul_(curator)" target="_blank">Christiane Paul</a> | (<a href="https://whitney.org/artport" target="_blank">video source</a>)</li>
+        <li><a href="https://www.newrafael.com/" target="_blank">Rafaël Rozendaal</a> | (<a href="https://www.youtube.com/watch?v=q2PlTV-RvnE" target="_blank">video source</a>)</li>
+        <li><a href="https://diana-adrianne.com/" target="_blank">Diana Smith</a> | (<a href="https://www.vice.com/en/article/9kgx7p/painting-made-with-code-html-pure-css-browser-art-diana-smith" target="_blank">video source</a>)</li>
+      </ul>
+    `
+  }
+]
+
+window.slides = slides

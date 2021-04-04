06a15fc06e2df9d2886b6b4d32303ea2ac380728
nbriz <nickbriz@gmail.com>
2021-04-04 15:28:17 -0500

    added assignment submission form

--- my_modules/routes.js
+++ my_modules/routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const router = express.Router()
 const path = require('path')
+const fs = require('fs')
 
 router.use(bodyParser.json({ limit: '10mb' }))
 
@@ -12,6 +13,28 @@ router.get('/readme', (req, res) => {
 router.post('/api/test', (req, res) => {
   const message = `u said ${req.body.message}`
   res.json({ message })
+})
+
+// nick's Internet Art II class assignment submissions end point
+router.post('/api/netart2/assignment', (req, res) => {
+  let message = 'thnx! your work has been submitted'
+  const dbpath = '../data/classes/netart2-spring2021.json'
+  const db = require(path.join(__dirname, dbpath))
+  if (!db) {
+    message = 'oops! there was an issue with the database :('
+    res.json({ message })
+  } else if (!req.body.name || !db[req.body.name]) {
+    message = 'oops! that username is not in the database :('
+    res.json({ message })
+  } else {
+    const data = { url: req.body.url, type: req.body.assignment }
+    db[req.body.name].push(data)
+    const dbJSON = JSON.stringify(db, null, 2)
+    fs.writeFile(path.join(__dirname, dbpath), dbJSON, (err) => {
+      if (err) res.json({ message: 'oops! server error :(' })
+      else res.json({ message })
+    })
+  }
 })
 
 module.exports = router
---
+++ www/classes/netart2/js/submit.js
@@ -0,0 +1,63 @@
+const students = [
+  'Thor',
+  'Camilla',
+  'Olivia',
+  'Michelle',
+  'Max',
+  'Mickey',
+  'Ursula',
+  'Amber',
+  'Nathan',
+  'Simren',
+  'Hakim',
+  'Maddie',
+  'Khalil',
+  'Alex',
+  'Camrick',
+  'Giacomo'
+]
+
+function invalid (url, name, assign) {
+  if (!url || url === '') return 'missing URL'
+  if (!name || name === '') return 'missing name'
+  if (!students.includes(name)) return `${name} is not a student in this class`
+  if (assign === 'assignment') return 'choose an assignment from the drop down list'
+  if (url.indexOf('https://github.com') !== 0) {
+    return 'the URL entered is not a valid github URL'
+  } else return false
+}
+
+function success (res) {
+  if (res.message === 'thnx! your work has been submitted') {
+    const url = document.querySelector('#url')
+    const name = document.querySelector('#name')
+    const assignment = document.querySelector('#assignment')
+    url.value = ''
+    name.value = ''
+    assignment.value = 'assignment'
+  }
+  window.alert(res.message)
+}
+
+function submit () {
+  const url = document.querySelector('#url').value
+  const name = document.querySelector('#name').value
+  const assignment = document.querySelector('#assignment').value
+  const err = invalid(url, name, assignment)
+  if (err) return window.alert(err)
+  const opts = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json, text/plain, */*',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ url, name, assignment })
+  }
+  window.fetch('/api/netart2/assignment', opts)
+    .then(res => res.json())
+    .then(res => success(res))
+    .catch(err => window.alert(err))
+}
+
+const submitBtn = document.querySelector('#submit')
+submitBtn.addEventListener('click', submit)

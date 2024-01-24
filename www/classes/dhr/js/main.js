3be3846990a486a913a4a859380ce2033693b706
nbriz <nickbriz@protonmail.com>
2024-01-24 16:57:43 -0600

    debug

--- www/classes/dhr/js/main.js
+++ www/classes/dhr/js/main.js
@@ -50,7 +50,7 @@ ne.cm.on('keydown', (cm, e) => window.numChange(e))
 // •.¸¸¸.•*•.¸¸¸.•*•.¸¸¸.•*•.¸¸¸.•*•.¸¸¸.•*•.¸¸¸.•* socket events
 
 socket.on('editor-init', (data) => {
-  console.log(data, group, data[group].code)
+  console.log(data, group, data[group], Object.keys(data[group]))
   ne.code = data[group].code
 })
 
